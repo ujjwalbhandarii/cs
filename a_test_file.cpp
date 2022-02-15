20bc05cff47f4d2317c553a8e27e6f0ce9e3e747
ujjwalbhandari14@gmail.com <98648115+ar3s0@users.noreply.github.com>
2022-02-15 20:35:28 +0545

    feb-15

--- a_test_file.cpp
+++ a_test_file.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-    cout << "welcome!";
+    cout << "test file";
     return 0;
 }
--- constructors.cpp
+++ constructors.cpp
@@ -20,6 +20,74 @@ int main()
     return 0;
 }
 
+// another
+#include <iostream>
+using namespace std;
+class sid
+{
+public:
+    int money;
+    sid(int a)
+    {
+        money = a;
+    }
+};
+int main()
+{
+    sid ujjwal(200);
+    cout << "ujjwal have to give " << ujjwal.money << " to sid";
+    return 0;
+}
+
+/*
+Funny one
+
+*/
+
+#include <iostream>
+using namespace std;
+class kiran
+{
+public:
+    string name;
+    int yesno;
+    kiran()
+    {
+
+        cout << "Enter your name: ";
+        cin >> name;
+    }
+    int ask();
+    int decision();
+};
+
+int kiran::ask()
+{
+    cout << "Hey " << name << "!" << endl;
+    cout << "Did you got your valentine?" << endl;
+    cout << "1. Yes \n2. Still searching" << endl;
+    cout << "Don't be shy tell tell ";
+    cin >> yesno;
+}
+int kiran::decision()
+{
+    if (yesno == 1)
+    {
+        cout << "congrats! enjoy your life bro." << endl;
+    }
+    if (yesno == 2)
+    {
+        cout << "Dont Worry you will soon get one. \nlife is not so easy but also not too hard. " << endl;
+    }
+}
+int main()
+{
+    kiran life;
+    life.ask();
+    life.decision();
+    return 0;
+}
+
 // constructors
 #include <iostream>
 using namespace std;
