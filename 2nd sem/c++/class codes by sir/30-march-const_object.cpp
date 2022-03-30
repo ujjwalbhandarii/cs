31e5d933b2ecdf3668bfaf747b485c3dca4f1d1f
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-03-30 17:21:05 +0545

    const object

---
+++ 2nd sem/c++/class codes by sir/30-march-const_object.cpp
@@ -0,0 +1,23 @@
+// const object
+#include <iostream>
+using namespace std;
+class test
+{
+public:
+    int x;
+    void showdata()
+    {
+        cout << x;
+    }
+};
+
+int main()
+{
+    const test t1;
+    t1.x = 50;
+    t1.showdata();
+
+    t1.x = 70;
+    t1.showdata();
+    return 0;
+}
