e9a75738b6c59d1ddd67863cb2c6b0f51d792958
ujjwalbhandari14 <ujjwalbhandari14@gmail.com>
2022-04-08 19:20:58 +0545

    opeartor overloading

---
+++ 2nd sem/c++/class codes by sir/1-april-opeartor_overloading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class rectangle
+{
+    int length, breadth;
+
+public:
+    rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    void operator++()
+    {
+        ++length;
+        ++breadth;
+    }
+
+    void display()
+    {
+        cout << length << "\t" << breadth << endl;
+    }
+};
+int main()
+{
+    rectangle r1(10, 11);
+    r1.display();
+    ++r1;
+    r1.display();
+    return 0;
+}
